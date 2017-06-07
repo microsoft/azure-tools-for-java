cdc6470090d3a1d64e30e00aea2f20b20315884b
Eskibear <me@eskibear.info>
2017-06-07 22:43:27 +0800

    fix indent

--- PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.container/src/com/microsoft/azuretools/container/handlers/DockerizeHandler.java
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.container/src/com/microsoft/azuretools/container/handlers/DockerizeHandler.java
@@ -30,6 +30,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.microsoft.azuretools.container.Constant;
 import com.microsoft.azuretools.core.utils.AzureAbstractHandler;
@@ -37,30 +38,30 @@ import com.microsoft.azuretools.core.utils.PluginUtil;
 
 public class DockerizeHandler extends AzureAbstractHandler {
 
-	@Override
-	public Object onExecute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-		IProject project = PluginUtil.getSelectedProject();
-	    IFolder folder = project.getFolder(Constant.DOCKER_CONTEXT_FOLDER);
-		try {
-		    if(!folder.exists()) folder.create(true, true, null);
-			createDockerFile(project, folder, Constant.DOCKERFILE_NAME);
-			MessageDialog.openInformation(window.getShell(), "Add Docker Support", String.format("%s\n\n%s", Constant.MESSAGE_DOCKERFILE_CREATED, Constant.MESSAGE_INSTRUCTION));
-		} catch (CoreException e) {
-			e.printStackTrace();
-			MessageDialog.openError(window.getShell(), this.getClass().toString(), Constant.ERROR_CREATING_DOCKERFILE);
-		}
-		return null;
-	}
-	
-	public void createDockerFile(IProject project, IFolder folder, String filename) throws CoreException{
-		//create file
-		IFile dockerfile = folder.getFile(filename);
-		if (!dockerfile.exists()) {
-		    byte[] bytes = String.format(Constant.DOCKERFILE_CONTENT_TOMCAT, project.getName()).getBytes();
-		    dockerfile.create(new ByteArrayInputStream(bytes), false, null);
-		}
-		
-	}
+    @Override
+    public Object onExecute(ExecutionEvent event) throws ExecutionException {
+        try {
+            IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+            IProject project = PluginUtil.getSelectedProject();
+            IFolder folder = project.getFolder(Constant.DOCKER_CONTEXT_FOLDER);
+            if(!folder.exists()) folder.create(true, true, null);
+            createDockerFile(project, folder, Constant.DOCKERFILE_NAME);
+            MessageDialog.openInformation(window.getShell(), "Add Docker Support", String.format("%s\n\n%s", Constant.MESSAGE_DOCKERFILE_CREATED, Constant.MESSAGE_INSTRUCTION));
+        } catch (CoreException e) {
+            e.printStackTrace();
+            MessageDialog.openError(window.getShell(), this.getClass().toString(), Constant.ERROR_CREATING_DOCKERFILE);
+        }
+        return null;
+    }
+    
+    public void createDockerFile(IProject project, IFolder folder, String filename) throws CoreException{
+        //create file
+        IFile dockerfile = folder.getFile(filename);
+        if (!dockerfile.exists()) {
+            byte[] bytes = String.format(Constant.DOCKERFILE_CONTENT_TOMCAT, project.getName()).getBytes();
+            dockerfile.create(new ByteArrayInputStream(bytes), false, null);
+        }
+        
+    }
 
 }
