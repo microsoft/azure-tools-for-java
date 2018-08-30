20b98e3553ffd9d370fb2fc73d0fcf91e9869a31
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2018-08-31 01:28:03 +0300

    overwrite /etc/hosts

--- teamcity.sh
+++ teamcity.sh
@@ -1,2 +1,2 @@
-echo "127.0.0.1 $(hostname)" >> /etc/hosts # without this spark-tools tests will fail in docker
+echo "127.0.0.1 $(hostname)" > /etc/hosts # without this spark-tools tests will fail in docker
 ./BuildDevint -B
