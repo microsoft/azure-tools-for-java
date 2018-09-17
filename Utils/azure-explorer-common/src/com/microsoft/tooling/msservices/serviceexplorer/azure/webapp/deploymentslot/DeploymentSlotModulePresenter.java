d50964519756d156072eae2c13e145a253f10108
Summer Sun <victoriasqq@gmail.com>
2018-09-17 16:40:59 +0800

    Do not freeze UI when getting deployment slots

--- Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/webapp/deploymentslot/DeploymentSlotModulePresenter.java
+++ Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/webapp/deploymentslot/DeploymentSlotModulePresenter.java
@@ -2,13 +2,31 @@ package com.microsoft.tooling.msservices.serviceexplorer.azure.webapp.deployment
 
 import com.microsoft.azuretools.core.mvp.model.webapp.AzureWebAppMvpModel;
 import com.microsoft.azuretools.core.mvp.ui.base.MvpPresenter;
-import java.io.IOException;
+import com.microsoft.tooling.msservices.components.DefaultLoader;
+
+import rx.Observable;
 
 public class DeploymentSlotModulePresenter<V extends DeploymentSlotModuleView> extends MvpPresenter<V> {
-    public void onRefreshDeploymentSlotModule(final String subscriptionId, final String webAppId) throws IOException {
-        final DeploymentSlotModuleView view = getMvpView();
-        if (view != null) {
-            view.renderDeploymentSlots(AzureWebAppMvpModel.getInstance().getDeploymentSlots(subscriptionId, webAppId));
-        }
+    private static final String CANNOT_GET_DEPLOYMENT_SLOTS = "Cannot get deployment slots.";
+
+    public void onRefreshDeploymentSlotModule(final String subscriptionId, final String webAppId) {
+        Observable.fromCallable(() -> AzureWebAppMvpModel.getInstance().getDeploymentSlots(subscriptionId, webAppId))
+            .subscribeOn(getSchedulerProvider().io())
+            .subscribe(deploymentSlots -> DefaultLoader.getIdeHelper().invokeLater(() -> {
+                if (isViewDetached()) {
+                    return;
+                }
+                getMvpView().renderDeploymentSlots(deploymentSlots);
+                // todo improve the error handling
+            }), e -> errorHandler(CANNOT_GET_DEPLOYMENT_SLOTS, (Exception) e));
+    }
+
+    private void errorHandler(final String msg, final Exception e) {
+        DefaultLoader.getIdeHelper().invokeLater(() -> {
+            if (isViewDetached()) {
+                return;
+            }
+            getMvpView().onErrorWithException(msg, e);
+        });
     }
 }
