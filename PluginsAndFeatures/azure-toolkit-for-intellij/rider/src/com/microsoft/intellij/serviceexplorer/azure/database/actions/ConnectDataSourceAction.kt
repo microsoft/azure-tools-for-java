618130159d8c49ce74d4a0543da0b0e6d60ac276
Maarten Balliauw <maarten@maartenballiauw.be>
2018-11-14 09:59:08 +0100

    Add newlines where needed

--- PluginsAndFeatures/azure-toolkit-for-intellij/rider/src/com/microsoft/intellij/serviceexplorer/azure/database/actions/ConnectDataSourceAction.kt
+++ PluginsAndFeatures/azure-toolkit-for-intellij/rider/src/com/microsoft/intellij/serviceexplorer/azure/database/actions/ConnectDataSourceAction.kt
@@ -18,7 +18,8 @@
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- */package com.microsoft.intellij.serviceexplorer.azure.database.actions
+ */
+package com.microsoft.intellij.serviceexplorer.azure.database.actions
 
 import com.intellij.database.autoconfig.DataSourceDetector
 import com.intellij.database.autoconfig.DataSourceRegistry
