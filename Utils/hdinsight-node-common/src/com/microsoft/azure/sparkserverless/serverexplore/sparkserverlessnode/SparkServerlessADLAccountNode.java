97ce7fa124d03ce1efc36c4a855c4fbae45022c4
Kun Huang (COSMOS) <kuhuan@microsoft.com>
2018-11-20 06:09:49 -0800

    add TODO

--- Utils/hdinsight-node-common/src/com/microsoft/azure/sparkserverless/serverexplore/sparkserverlessnode/SparkServerlessADLAccountNode.java
+++ Utils/hdinsight-node-common/src/com/microsoft/azure/sparkserverless/serverexplore/sparkserverlessnode/SparkServerlessADLAccountNode.java
@@ -80,6 +80,7 @@ public class SparkServerlessADLAccountNode extends AzureRefreshableNode implemen
     protected void loadActions() {
         super.loadActions();
 
+        // TODO: enable after testing with backend ready
         /*addAction("Submit Serverless Spark Job", new CosmosSparkServerlessSubmitAction(
                 this, adlAccount, SparkServerlessClusterOps.getInstance().getServerlessSubmitAction()));*/
 
