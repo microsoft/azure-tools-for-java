ab1c93f7a6a7204f2e2e5149c9f2c033b5a51893
Sergey Dubov <sergey.dubov@jetbrains.com>
2018-12-21 19:00:00 +0300

    Fix #133

--- Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/database/AzureDatabaseModule.kt
+++ Utils/azure-explorer-common/src/com/microsoft/tooling/msservices/serviceexplorer/azure/database/AzureDatabaseModule.kt
@@ -31,7 +31,7 @@ class AzureDatabaseModule(parent: Node) : AzureRefreshableNode(MODULE_ID, BASE_M
     companion object {
         private val MODULE_ID = AzureDatabaseModule::class.java.name
         private const val ICON_PATH = "Database.svg"
-        private const val BASE_MODULE_NAME = "Sql Databases"
+        private const val BASE_MODULE_NAME = "SQL Databases"
         private const val ERROR_DELETE_SQL_SERVER = "An error occurred while attempting to delete the SQL Server: %s"
     }
 
