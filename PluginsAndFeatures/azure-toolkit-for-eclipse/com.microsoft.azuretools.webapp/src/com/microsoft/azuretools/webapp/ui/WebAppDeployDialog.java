1be598a3a570dcf8e8cfb2cec2585c435cab580e
pensh <pensh@microsoft.com>
2019-02-28 10:09:00 +0800

    Do not use wildcard import

--- PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.webapp/src/com/microsoft/azuretools/webapp/ui/WebAppDeployDialog.java
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.webapp/src/com/microsoft/azuretools/webapp/ui/WebAppDeployDialog.java
@@ -2,7 +2,12 @@ package com.microsoft.azuretools.webapp.ui;
 
 import java.io.InputStream;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 
 import com.microsoft.azure.management.appservice.*;
