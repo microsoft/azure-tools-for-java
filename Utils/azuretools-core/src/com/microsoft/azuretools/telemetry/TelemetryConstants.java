e2faf2f07b415416bd154385528b058b19996c36
zmssp <peng.shi@800os.cn>
2019-03-18 16:28:27 +0800

    Add copyright header and code refactor

--- Utils/azuretools-core/src/com/microsoft/azuretools/telemetry/TelemetryConstants.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/telemetry/TelemetryConstants.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.microsoft.azuretools.telemetry;
 
 public class TelemetryConstants {
@@ -10,8 +32,8 @@ public class TelemetryConstants {
     public static final String CREATE_WEB_APP = "create-webapp";
     public static final String DELETE_WEB_APP = "delete-webapp";
     public static final String DEPLOY_WEB_APP = "deploy-webapp";
-    public static final String DEPLOY_DEPLOYMENT_SLOT = "deploy-to-slot";
-    public static final String CREATE_DEPLOYMENT_SLOT = "create-deployment-slot";
+    public static final String DEPLOY_DEPLOYMENT_SLOT = "deploy-webapp-slot";
+    public static final String CREATE_DEPLOYMENT_SLOT = "create-webapp-slot";
     public static final String OPEN_CREATEWEBAPP_DIALOG = "open-create-webapp-dialog";
     public static final String REFRESH_METADATA = "refresh";
 }
--- Utils/azuretools-core/src/com/microsoft/azuretools/telemetrywrapper/DefaultProducer.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/telemetrywrapper/DefaultProducer.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.microsoft.azuretools.telemetrywrapper;
 
 import com.microsoft.applicationinsights.TelemetryClient;
@@ -7,12 +29,14 @@ import java.util.UUID;
 
 public class DefaultProducer implements Producer {
     private static ThreadLocal<String> operIDS = new ThreadLocal<>();
+    private static ThreadLocal<Map<String, String>> errorInfo = new ThreadLocal<>();
     private static final String OPERATION_NAME = "operationName";
     private static final String OPERATION_ID = "operationId";
     private static final String ERROR_CODE = "errorCode";
     private static final String ERROR_MSG = "message";
     private static final String ERROR_TYPE = "errorType";
     private static final String DURATION = "duration";
+    private TelemetryClient client;
 
     @Override
     public void startTransaction(String eventName, String operName, Map<String, String> properties) {
@@ -29,9 +53,15 @@ public class DefaultProducer implements Producer {
         try {
             Map<String, Double> metrics = new HashMap<>();
             metrics.put(DURATION, Double.valueOf(time));
-            sendTelemetry(EventType.opEnd, eventName, mergeProperties(addOperNameAndId(properties, operName)), metrics);
-            operIDS.remove();
+            Map<String, String> mergedProperty = mergeProperties(addOperNameAndId(properties, operName));
+            if (errorInfo.get() != null) {
+                mergedProperty.putAll(errorInfo.get());
+            }
+            sendTelemetry(EventType.opEnd, eventName, mergedProperty, metrics);
         } catch (Exception ignore) {
+        } finally {
+            errorInfo.remove();
+            operIDS.remove();
         }
     }
 
@@ -39,10 +69,17 @@ public class DefaultProducer implements Producer {
     public void sendError(String eventName, String operName, ErrorType errorType, String errMsg,
         Map<String, String> properties) {
         try {
+            Map<String, String> errorMap = new HashMap<>();
+            errorMap.put(ERROR_CODE, "1");
+            errorMap.put(ERROR_MSG, errMsg);
+            errorMap.put(ERROR_TYPE, errorType.name());
+            // we need to save errorinfo, and then write the error info when we end the transaction, by this way we
+            // can quickly get the operation result from opend
+            errorInfo.remove();
+            errorInfo.set(errorMap);
+
             Map<String, String> newProperties = addOperNameAndId(properties, operName);
-            newProperties.put(ERROR_CODE, "1");
-            newProperties.put(ERROR_MSG, errMsg);
-            newProperties.put(ERROR_TYPE, errorType.name());
+            newProperties.putAll(errorMap);
             sendTelemetry(EventType.error, eventName, mergeProperties(newProperties), null);
         } catch (Exception ignore) {
         }
@@ -62,6 +99,11 @@ public class DefaultProducer implements Producer {
             sendTelemetry(EventType.warn, eventName, mergeProperties(addOperNameAndId(properties, operName)), null);
         } catch (Exception ignore) {
         }
+    }
+
+    @Override
+    public void setTelemetryClient(TelemetryClient client) {
+        this.client = client;
     }
 
     private Map<String, String> addOperNameAndId(Map<String, String> properties, String operName) {
@@ -88,10 +130,9 @@ public class DefaultProducer implements Producer {
 
     private synchronized void sendTelemetry(EventType eventType, String eventName, Map<String, String> properties,
         Map<String, Double> metrics) {
-        TelemetryClient telemetry = TelemetryManager.getInstance().getTelemetryClient();
-        if (telemetry != null) {
-            telemetry.trackEvent(getFullEventName(eventName, eventType), properties, metrics);
-            telemetry.flush();
+        if (client != null) {
+            client.trackEvent(getFullEventName(eventName, eventType), properties, metrics);
+            client.flush();
         }
     }
 
