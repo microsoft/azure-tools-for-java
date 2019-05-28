4df9b2e9c328a1f5e83a7e62608b58725953fc56
Rui Fang <rufan@microsoft.com>
2019-05-28 11:01:11 +0800

    Send telemetry when select path in VFS of ADLS Gen2

--- Utils/azuretools-core/src/com/microsoft/azuretools/telemetry/TelemetryConstants.java
+++ Utils/azuretools-core/src/com/microsoft/azuretools/telemetry/TelemetryConstants.java
@@ -39,6 +39,7 @@ public class TelemetryConstants {
     public static final String ACTION = "action";
     public static final String DIALOG = "dialog";
     public static final String HDINSIGHT = "hdinsight";
+    public static final String VFS = "vfs";    // virtual file system
 
     // operation value
     public static final String FEEDBACK = "feedback";
@@ -119,6 +120,7 @@ public class TelemetryConstants {
     public static final String ACR_READPROP = "readprop-acr";
     public static final String SIGNIN_DC = "dc";
     public static final String SIGNIN_SP = "sp";
+    public static final String SELECT_PATH_GEN2 = "select-path-gen2";
 
     // property name
     public static final String WEBAPP_DEPLOY_TO_SLOT = "webappDeployToSlot";
