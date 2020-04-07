c8de3e8d0e865c144f8c7a3a5dfdfd7f8aaee521
Hanxiao Liu <hanli@microsoft.com>
2020-04-07 20:16:53 +0800

    Add support for function streaming log

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/intellij/helpers/function/FunctionStreamingLogManager.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.microsoft.intellij.helpers.function;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.microsoft.azure.management.appservice.FunctionApp;
+import com.microsoft.azure.management.appservice.OperatingSystem;
+import com.microsoft.azuretools.core.mvp.model.function.AzureFunctionMvpModel;
+import com.microsoft.intellij.helpers.StreamingLogsToolWindowManager;
+import com.microsoft.intellij.util.PluginUtil;
+import com.microsoft.tooling.msservices.components.DefaultLoader;
+import org.jetbrains.annotations.NotNull;
+import rx.Observable;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public enum FunctionStreamingLogManager {
+    INSTANCE;
+
+    public static final String STREAMING_LOG_NOT_STARTED = "Streaming log is not started.";
+
+    private Map<String, FunctionStreamingLogConsoleView> consoleViewMap = new HashMap<>();
+    private Map<Project, ToolWindow> toolWindowMap = new HashMap<>();
+
+    public void showStreamingLog(Project project, String subscriptionId, String appName, String appId) {
+        DefaultLoader.getIdeHelper().runInBackground(project, "Starting Streaming Log...", false, true, null, () -> {
+            try {
+                if (isLinuxFunction(subscriptionId, appId)) {
+                    // Todo: Open portal Application Insight pages for function logging
+                    ApplicationManager.getApplication().invokeLater(() -> PluginUtil.displayInfoDialog(
+                            "Not supported",
+                            "Log streaming for Linux Function App is not supported in current version."));
+                    return;
+                }
+                FunctionStreamingLogConsoleView consoleView = consoleViewMap.get(appId);
+                if (consoleView == null) {
+                    consoleView = new FunctionStreamingLogConsoleView(project, appId);
+                }
+                if (!consoleView.isEnable()) {
+                    FunctionApp functionApp = AzureFunctionMvpModel.getInstance()
+                                                                   .getFunctionById(subscriptionId, appId);
+                    // Todo: Check Whether file logging is enabled and prompt user to enable it
+                    Observable<String> log = functionApp.streamAllLogsAsync();
+                    consoleView.startStreamingLog(log);
+                }
+                consoleViewMap.put(appId, consoleView);
+                StreamingLogsToolWindowManager.getInstance().showStreamingLogConsole(
+                        project, appId, String.format("%s-%s", appName, appName), consoleView);
+            } catch (Throwable e) {
+                ApplicationManager.getApplication().invokeLater(() -> PluginUtil.displayErrorDialog(
+                        "Failed to start streaming log",
+                        e.getMessage()));
+            }
+        });
+    }
+
+    public void closeStreamingLog(Project project, String subscriptionId, String appName, String appId) {
+        ProgressManager.getInstance().run(new Task.Backgroundable(null, "Closing Streaming Log...", false) {
+            @Override
+            public void run(@NotNull ProgressIndicator progressIndicator) {
+                if (consoleViewMap.containsKey(appId) && consoleViewMap.get(appId).isEnable()) {
+                    final FunctionStreamingLogConsoleView consoleView = consoleViewMap.get(appId);
+                    consoleView.closeStreamingLog();
+                } else {
+                    ApplicationManager.getApplication().invokeLater(() -> PluginUtil.displayErrorDialog(
+                            "Failed to close streaming log", STREAMING_LOG_NOT_STARTED));
+                }
+            }
+        });
+    }
+
+    public void removeConsoleView(String resourceId) {
+        consoleViewMap.remove(resourceId);
+    }
+
+    private boolean isLinuxFunction(String subscription, String functionId) throws IOException {
+        FunctionApp functionApp = AzureFunctionMvpModel.getInstance().getFunctionById(subscription, functionId);
+        return functionApp.operatingSystem() == OperatingSystem.LINUX;
+    }
+
+}
