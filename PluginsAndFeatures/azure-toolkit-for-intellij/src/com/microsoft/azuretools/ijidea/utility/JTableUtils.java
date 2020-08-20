4694099bb725f9e8a0c09a1184a1fc4d167d6368
Mingliang Wang <wangmi@microsoft.com>
2020-08-20 12:40:17 +0800

    #4509: [UI/UX]use ThreeStateCheckbox instead to provide better UI/UX

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azuretools/ijidea/utility/JTableUtils.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azuretools/ijidea/utility/JTableUtils.java
@@ -1,121 +1,84 @@
 package com.microsoft.azuretools.ijidea.utility;
 
+import com.intellij.profile.codeInspection.ui.table.ThreeStateCheckBoxRenderer;
+
 import javax.swing.*;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.JTableHeader;
-import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableModel;
-import java.awt.*;
 import java.awt.event.*;
+import java.util.Objects;
 
 public class JTableUtils {
     public static void enableBatchSelection(JTable table, int checkboxColumnIndex) {
         final TableColumn column = table.getColumnModel().getColumn(checkboxColumnIndex);
-        final CheckBoxHeaderRenderer checkboxHeader = new CheckBoxHeaderRenderer();
-        column.setHeaderRenderer(checkboxHeader);
-        checkboxHeader.addItemListener(new CheckboxHeaderStateListener(table, checkboxColumnIndex));
-//        table.getModel().addTableModelListener(new CheckBoxModelListener(table, checkboxColumnIndex));
+        final CheckBoxModelListener listener = new CheckBoxModelListener(table, checkboxColumnIndex);
+        column.setHeaderValue(true);
+        column.setHeaderRenderer(new ThreeStateCheckBoxRenderer());
+        table.getModel().addTableModelListener(listener);
     }
 
-    private static class CheckBoxModelListener implements TableModelListener, ItemListener {
+    private static class CheckBoxModelListener extends MouseAdapter implements TableModelListener {
         private final JTable table;
         private final int colIndex;
-        private final JCheckBox checkbox;
+        private boolean changingHeaderValue = false;
 
         public CheckBoxModelListener(final JTable table, final int columnIndex) {
+            super();
             this.table = table;
             this.colIndex = columnIndex;
-            final TableColumn column = table.getColumnModel().getColumn(columnIndex);
-            this.checkbox = (JCheckBox) column.getHeaderRenderer();
-            this.checkbox.addItemListener(this);
+            final JTableHeader header = table.getTableHeader();
+            header.addMouseListener(this);
         }
 
         @Override
         public void tableChanged(final TableModelEvent e) {
             final int column = e.getColumn();
             final TableModel model = (TableModel) e.getSource();
-            if (column == this.colIndex) {
-                boolean allSelected = true;
+            if (column == this.colIndex && !this.changingHeaderValue) {
+                int numSelected = 0;
                 for (int row = 0; row < model.getRowCount(); row++) {
-                    allSelected = allSelected && (Boolean) model.getValueAt(row, this.colIndex);
+                    if ((Boolean) model.getValueAt(row, this.colIndex)) {
+                        numSelected++;
+                    }
                 }
-                this.checkbox.getModel().setSelected(allSelected);
+                this.updateHeaderState(numSelected);
             }
+        }
+
+        private void updateHeaderState(final int numSelected) {
+            final TableColumn column = table.getColumnModel().getColumn(this.colIndex);
+            if (numSelected == this.table.getModel().getRowCount()) {
+                column.setHeaderValue(true);
+            } else if (numSelected == 0) {
+                column.setHeaderValue(false);
+            } else {
+                column.setHeaderValue(null);
+            }
+            this.table.getTableHeader().repaint();
         }
 
         @Override
-        public void itemStateChanged(ItemEvent e) {
-            final Object source = e.getSource();
-            if (!(source instanceof AbstractButton)) {
-                return;
-            }
-            final boolean checked = e.getStateChange() == ItemEvent.SELECTED;
-            final int rowCount = table.getModel().getRowCount();
-            for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
-                table.getModel().setValueAt(checked, rowIndex, this.colIndex);
-            }
-        }
-    }
-
-    static class CheckBoxHeaderRenderer extends JCheckBox implements TableCellRenderer, MouseListener {
-        private int column = -1;
-
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-            if (this.column == -1) {
-                this.column = column;
-                final JTableHeader header = table.getTableHeader();
-                this.setForeground(header.getForeground());
-                this.setBackground(header.getBackground());
-                header.addMouseListener(this);
-            }
-            return this;
-        }
-
         public void mouseClicked(MouseEvent e) {
             final JTableHeader header = (JTableHeader) (e.getSource());
             final JTable table = header.getTable();
             final int columnIndex = table.columnAtPoint(e.getPoint());
 
-            if (columnIndex == this.column) {
-                doClick();
+            if (columnIndex == this.colIndex) {
+                final TableColumn checkboxColumn = table.getColumnModel().getColumn(this.colIndex);
+                final Object value = checkboxColumn.getHeaderValue();
+                final boolean checked = Objects.nonNull(value) && (boolean) value;
+                final int rowCount = table.getModel().getRowCount();
+                this.changingHeaderValue = true;
+                checkboxColumn.setHeaderValue(!checked);
+                for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+                    table.getModel().setValueAt(!checked, rowIndex, this.colIndex);
+                }
+                this.changingHeaderValue = false;
             }
             header.repaint();
-        }
-
-        public void mousePressed(MouseEvent e) {
-        }
-
-        public void mouseReleased(MouseEvent e) {
-        }
-
-        public void mouseEntered(MouseEvent e) {
-        }
-
-        public void mouseExited(MouseEvent e) {
-        }
-    }
-
-    private static class CheckboxHeaderStateListener implements ItemListener {
-        private final JTable table;
-        private final int checkboxColumnIndex;
-
-        public CheckboxHeaderStateListener(JTable table, int checkboxColumnIndex) {
-            this.table = table;
-            this.checkboxColumnIndex = checkboxColumnIndex;
-        }
-
-        public void itemStateChanged(ItemEvent e) {
-            final Object source = e.getSource();
-            if (!(source instanceof AbstractButton)) {
-                return;
-            }
-            final boolean checked = e.getStateChange() == ItemEvent.SELECTED;
-            final int rowCount = table.getModel().getRowCount();
-            for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
-                table.getModel().setValueAt(checked, rowIndex, this.checkboxColumnIndex);
-            }
         }
     }
 }
