966eeaf4a0240172ef56bc34bf5a2995e6571818
Mingliang Wang <wangmi@microsoft.com>
2020-10-12 23:52:04 +0800

    convert WebAppConfig to WebAppSettingModel

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/appservice/webapp/WebAppService.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/appservice/webapp/WebAppService.java
@@ -1,9 +1,12 @@
 package com.microsoft.azure.appservice.webapp;
 
+import com.microsoft.azure.management.appservice.JavaVersion;
+import com.microsoft.azure.management.appservice.OperatingSystem;
 import com.microsoft.azure.management.appservice.WebApp;
 import com.microsoft.azuretools.core.mvp.model.webapp.AzureWebAppMvpModel;
 import com.microsoft.azuretools.core.mvp.model.webapp.WebAppSettingModel;
 import com.microsoft.azuretools.telemetrywrapper.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 
@@ -19,6 +22,7 @@ public class WebAppService {
 
     public WebApp createWebApp(final WebAppConfig config) throws Exception {
         final WebAppSettingModel settings = convertConfig2Settings(config);
+        settings.setCreatingNew(true);
         final Map<String, String> properties = settings.getTelemetryProperties(null);
         final Operation operation = TelemetryManager.createOperation(WEBAPP, CREATE_WEBAPP);
         try {
@@ -33,7 +37,26 @@ public class WebAppService {
         }
     }
 
-    private static WebAppSettingModel convertConfig2Settings(final WebAppConfig config) {
-        return null;
+    public static WebAppSettingModel convertConfig2Settings(final WebAppConfig config) {
+        final WebAppSettingModel settings = new WebAppSettingModel();
+        settings.setSubscriptionId(config.getSubscription().subscriptionId());
+        // creating if id is empty
+        settings.setCreatingResGrp(StringUtils.isEmpty(config.getResourceGroup().id()));
+        settings.setResourceGroup(config.getResourceGroup().name());
+        settings.setWebAppName(config.getName());
+        settings.setOS(config.getPlatform().getOs());
+        settings.setRegion(config.getRegion().name());
+        if (settings.getOS() == OperatingSystem.LINUX) {
+            settings.setStack(config.getPlatform().getStackOrWebContainer());
+            settings.setVersion(config.getPlatform().getStackVersionOrJavaVersion());
+        } else if (settings.getOS() == OperatingSystem.WINDOWS) {
+            settings.setWebContainer(config.getPlatform().getStackOrWebContainer());
+            settings.setJdkVersion(JavaVersion.fromString(config.getPlatform().getStackVersionOrJavaVersion()));
+        }
+        // creating if id is empty
+        settings.setCreatingAppServicePlan(StringUtils.isEmpty(config.getServicePlan().id()));
+        settings.setAppServicePlanId(config.getServicePlan().id());
+        settings.setPricing(config.getServicePlan().pricingTier().toString());
+        return settings;
     }
 }
