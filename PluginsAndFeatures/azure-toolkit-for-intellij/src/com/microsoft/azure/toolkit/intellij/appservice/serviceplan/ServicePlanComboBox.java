faf3fbad5bcf47c1c60641aa44f2c028f1f7a32e
Mingliang Wang <wangmi@microsoft.com>
2020-10-18 00:22:27 +0800

    Task 1784218: filter service plan by subscription & region & os

--- PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/appservice/serviceplan/ServicePlanComboBox.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/src/com/microsoft/azure/toolkit/intellij/appservice/serviceplan/ServicePlanComboBox.java
@@ -25,24 +25,36 @@ package com.microsoft.azure.toolkit.intellij.appservice.serviceplan;
 import com.intellij.icons.AllIcons;
 import com.intellij.ui.components.fields.ExtendableTextComponent;
 import com.microsoft.azure.management.appservice.AppServicePlan;
+import com.microsoft.azure.management.appservice.OperatingSystem;
 import com.microsoft.azure.management.resources.Subscription;
+import com.microsoft.azure.management.resources.fluentcore.arm.Region;
 import com.microsoft.azure.toolkit.intellij.common.AzureComboBox;
+import com.microsoft.azure.toolkit.lib.appservice.Mock;
+import com.microsoft.azure.toolkit.lib.appservice.ServicePlanMock;
 import com.microsoft.azuretools.azurecommons.helpers.NotNull;
 import com.microsoft.azuretools.azurecommons.helpers.Nullable;
 import com.microsoft.azuretools.core.mvp.model.webapp.AzureWebAppMvpModel;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ServicePlanComboBox extends AzureComboBox<AppServicePlan> {
 
     private Subscription subscription;
+    private ServicePlanMock localItem;
+    private OperatingSystem os;
+    private Region region;
 
     @Override
     protected String getItemText(final Object item) {
         if (Objects.isNull(item)) {
             return EMPTY_ITEM;
+        }
+        if (item instanceof Mock) {
+            return "(New) " + ((AppServicePlan) item).name();
         }
         return ((AppServicePlan) item).name();
     }
@@ -56,24 +68,62 @@ public class ServicePlanComboBox extends AzureComboBox<AppServicePlan> {
         this.refreshItems();
     }
 
+    public void setOperatingSystem(OperatingSystem os) {
+        this.os = os;
+        if (os == null) {
+            this.clear();
+            return;
+        }
+        this.refreshItems();
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+        if (region == null) {
+            this.clear();
+            return;
+        }
+        this.refreshItems();
+    }
+
     @NotNull
     @Override
     protected List<? extends AppServicePlan> loadItems() throws Exception {
+        final List<AppServicePlan> plans = new ArrayList<>();
         if (Objects.nonNull(this.subscription)) {
             final String sid = subscription.subscriptionId();
-            return AzureWebAppMvpModel.getInstance().listAppServicePlanBySubscriptionId(sid);
+            Stream<AppServicePlan> stream = AzureWebAppMvpModel
+                .getInstance()
+                .listAppServicePlanBySubscriptionId(sid)
+                .stream();
+            if (Objects.nonNull(this.region)) {
+                stream = stream.filter(p -> Objects.equals(p.region(), this.region));
+            }
+            if (Objects.nonNull(this.os)) {
+                stream = stream.filter(p -> p.operatingSystem() == this.os);
+            }
+            plans.addAll(stream.collect(Collectors.toList()));
         }
-        return Collections.emptyList();
+        return plans;
     }
 
     @Nullable
     @Override
     protected ExtendableTextComponent.Extension getExtension() {
         return ExtendableTextComponent.Extension.create(
-                AllIcons.General.Add, "Create new service plan", this::showServicePlanCreationPopup);
+            AllIcons.General.Add, "Create new app service plan", this::showServicePlanCreationPopup);
     }
 
     private void showServicePlanCreationPopup() {
-
+        final ServicePlanCreationDialog dialog = new ServicePlanCreationDialog(this.subscription, this.os, this.region);
+        dialog.setOkActionListener((plan) -> {
+            this.localItem = plan;
+            dialog.close();
+            final List<AppServicePlan> items = this.getItems();
+            items.add(0, this.localItem);
+            this.setItems(items);
+            this.setValue(this.localItem);
+        });
+        dialog.show();
     }
 }
