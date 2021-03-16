3e08f945331a42b9f82182d8a246fe28efff717e
Wang Mingliang <wangmi@microsoft.com>
2021-03-16 23:49:24 +0800

    use cache to simplify implementation.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-sdk-reference-book/src/main/java/com/microsoft/azure/toolkit/intellij/azuresdk/service/AzureSdkLibraryService.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-sdk-reference-book/src/main/java/com/microsoft/azure/toolkit/intellij/azuresdk/service/AzureSdkLibraryService.java
@@ -21,8 +21,8 @@ public class AzureSdkLibraryService {
     private static final ObjectMapper mapper = new YAMLMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     private static final String SDK_METADATA_URL = "https://raw.githubusercontent.com/Azure/azure-sdk-for-java/master/sdk/spring/spring-reference.yml";
 
-    @Cacheable(value = "sdk-services", condition = "$force!=true")
-    public static List<AzureSdkServiceEntity> loadAzureSdkServices(boolean force) throws IOException {
+    @Cacheable(value = "sdk-services", condition = "!(force&&force[0])")
+    public static List<AzureSdkServiceEntity> loadAzureSdkServices(boolean... force) throws IOException {
         final URL destination = new URL(SDK_METADATA_URL);
         final ObjectReader reader = mapper.readerFor(AzureSdkServiceEntity.class);
         final MappingIterator<AzureSdkServiceEntity> data = reader.readValues(destination);
