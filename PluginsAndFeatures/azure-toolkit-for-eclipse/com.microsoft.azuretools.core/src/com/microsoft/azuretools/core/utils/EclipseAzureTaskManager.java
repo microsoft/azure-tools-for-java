8003e173229a9ffac80dc764d0f7ef0c870363a9
Mingliang Wang <wangmi@microsoft.com>
2021-09-06 15:11:07 +0800

    use spaces instead of tab.

--- PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.core/src/com/microsoft/azuretools/core/utils/EclipseAzureTaskManager.java
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.core/src/com/microsoft/azuretools/core/utils/EclipseAzureTaskManager.java
@@ -23,72 +23,72 @@ import java.util.concurrent.Executors;
 
 public class EclipseAzureTaskManager extends AzureTaskManager {
 
-	private static final ExecutorService executor = Executors.newFixedThreadPool(4);
+    private static final ExecutorService executor = Executors.newFixedThreadPool(4);
 
-	@Override
-	protected void doRead(Runnable runnable, final AzureTask<?> task) {
-		throw new UnsupportedOperationException("not support");
-	}
+    @Override
+    protected void doRead(Runnable runnable, final AzureTask<?> task) {
+        throw new UnsupportedOperationException("not support");
+    }
 
-	@Override
-	protected void doWrite(final Runnable runnable, final AzureTask<?> task) {
-		throw new UnsupportedOperationException("not support");
-	}
+    @Override
+    protected void doWrite(final Runnable runnable, final AzureTask<?> task) {
+        throw new UnsupportedOperationException("not support");
+    }
 
-	@Override
-	protected void doRunOnPooledThread(Runnable runnable, AzureTask<?> task) {
-		executor.submit(runnable);
-	}
+    @Override
+    protected void doRunOnPooledThread(Runnable runnable, AzureTask<?> task) {
+        executor.submit(runnable);
+    }
 
-	@Override
-	protected void doRunLater(Runnable runnable, AzureTask<?> task) {
-		Display.getDefault().asyncExec(runnable);
-	}
+    @Override
+    protected void doRunLater(Runnable runnable, AzureTask<?> task) {
+        Display.getDefault().asyncExec(runnable);
+    }
 
-	@Override
-	protected void doRunAndWait(Runnable runnable, AzureTask<?> task) {
-		Display.getDefault().syncExec(runnable);
-	}
+    @Override
+    protected void doRunAndWait(Runnable runnable, AzureTask<?> task) {
+        Display.getDefault().syncExec(runnable);
+    }
 
-	@Override
-	protected void doRunInBackground(Runnable runnable, AzureTask<?> task) {
-		final String title = String.format("Azure: %s...", Objects.requireNonNull(task.getTitle()));
-		Job.create(title, (monitor) -> {
-			monitor.beginTask(title, IProgressMonitor.UNKNOWN);
-			try {
-				task.setBackgrounded(true);
-				runnable.run();
-			} finally {
-				monitor.done();
-			}
-			return Status.OK_STATUS;
-		}).schedule();
-	}
+    @Override
+    protected void doRunInBackground(Runnable runnable, AzureTask<?> task) {
+        final String title = String.format("Azure: %s...", Objects.requireNonNull(task.getTitle()));
+        Job.create(title, (monitor) -> {
+            monitor.beginTask(title, IProgressMonitor.UNKNOWN);
+            try {
+                task.setBackgrounded(true);
+                runnable.run();
+            } finally {
+                monitor.done();
+            }
+            return Status.OK_STATUS;
+        }).schedule();
+    }
 
-	@Override
-	protected void doRunInModal(final Runnable runnable, final AzureTask<?> task) {
-		if (task.isBackgroundable()) {
-			this.doRunInBackground(runnable, task);
-		} else {
-			this.doRunInUnBackgroundableModal(runnable, task);
-		}
-	}
+    @Override
+    protected void doRunInModal(final Runnable runnable, final AzureTask<?> task) {
+        if (task.isBackgroundable()) {
+            this.doRunInBackground(runnable, task);
+        } else {
+            this.doRunInUnBackgroundableModal(runnable, task);
+        }
+    }
 
-	protected void doRunInUnBackgroundableModal(final Runnable runnable, final AzureTask<?> task) {
-		final String title = String.format("Azure: %s...", Objects.requireNonNull(task.getTitle()));
-		try {
-			new ProgressMonitorDialog(new Shell()).run(true, task.isCancellable(), monitor -> {
-				monitor.beginTask(title, IProgressMonitor.UNKNOWN);
-				try {
-					task.setBackgrounded(false);
-					runnable.run();
-				} finally {
-					monitor.done();
-				}
-			});
-		} catch (InvocationTargetException | InterruptedException e) {
-			String msg = String.format("failed to execute task (%s)", task.getTitle());
-			throw new AzureToolkitRuntimeException(msg, e);
-		}
-	}
+    protected void doRunInUnBackgroundableModal(final Runnable runnable, final AzureTask<?> task) {
+        final String title = String.format("Azure: %s...", Objects.requireNonNull(task.getTitle()));
+        try {
+            new ProgressMonitorDialog(new Shell()).run(true, task.isCancellable(), monitor -> {
+                monitor.beginTask(title, IProgressMonitor.UNKNOWN);
+                try {
+                    task.setBackgrounded(false);
+                    runnable.run();
+                } finally {
+                    monitor.done();
+                }
+            });
+        } catch (InvocationTargetException | InterruptedException e) {
+            String msg = String.format("failed to execute task (%s)", task.getTitle());
+            throw new AzureToolkitRuntimeException(msg, e);
+        }
+    }
 }
