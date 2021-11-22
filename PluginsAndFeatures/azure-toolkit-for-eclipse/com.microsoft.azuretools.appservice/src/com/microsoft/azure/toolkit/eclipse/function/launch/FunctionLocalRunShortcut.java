fa5ba64cda3a4fd9ee1e785ae4c614c59db3808a
Andy Xu(devdiv) <andxu@microsoft>
2021-11-22 22:40:43 +0800

    add initial shortcut

---
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.appservice/src/com/microsoft/azure/toolkit/eclipse/function/launch/FunctionLocalRunShortcut.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.eclipse.function.launch;
+
+import org.eclipse.debug.ui.ILaunchShortcut;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+
+public class FunctionLocalRunShortcut implements ILaunchShortcut {
+
+	@Override
+	public void launch(ISelection arg0, String arg1) {
+		
+	}
+
+	@Override
+	public void launch(IEditorPart arg0, String arg1) {
+		
+	}
+
+}
