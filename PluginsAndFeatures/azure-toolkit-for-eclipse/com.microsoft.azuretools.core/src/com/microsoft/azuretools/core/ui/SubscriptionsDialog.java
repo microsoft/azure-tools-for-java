7b2cb40c4fed8b1732d22c750c7fcaef0b1e1053
Hanxiao Liu <hanli@microsoft.com>
2024-05-07 13:32:49 +0800

    Use space instead of tab in code

--- PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.core/src/com/microsoft/azuretools/core/ui/SubscriptionsDialog.java
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.core/src/com/microsoft/azuretools/core/ui/SubscriptionsDialog.java
@@ -45,108 +45,108 @@ import static com.microsoft.azuretools.telemetry.TelemetryConstants.ACCOUNT;
 import static com.microsoft.azuretools.telemetry.TelemetryConstants.SELECT_SUBSCRIPTIONS;
 
 public class SubscriptionsDialog extends AzureTitleAreaDialogWrapper {
-	private static final String SUBSCRIPTION_MESSAGE = "Select subscription(s) you want to use.";
-	private static final String SUBSCRIPTION_TITLE = "Your Subscriptions";
-	private static final String DIALOG_TITLE = "Select Subscriptions";
-	private static ILog LOG = Activator.getDefault().getLog();
+    private static final String SUBSCRIPTION_MESSAGE = "Select subscription(s) you want to use.";
+    private static final String SUBSCRIPTION_TITLE = "Your Subscriptions";
+    private static final String DIALOG_TITLE = "Select Subscriptions";
+    private static ILog LOG = Activator.getDefault().getLog();
 
-	private Table table;
+    private Table table;
 
-	private SubscriptionManager subscriptionManager;
-	private List<SubscriptionDetail> sdl;
+    private SubscriptionManager subscriptionManager;
+    private List<SubscriptionDetail> sdl;
 
-	/**
-	 * Create the dialog.
-	 * @param parentShell
-	 */
-	private SubscriptionsDialog(Shell parentShell, SubscriptionManager subscriptionManage) {
-		super(parentShell);
-		setHelpAvailable(false);
-		setShellStyle(SWT.DIALOG_TRIM | SWT.RESIZE | SWT.APPLICATION_MODAL);
-		this.subscriptionManager = subscriptionManage;
-	}
+    /**
+     * Create the dialog.
+     * @param parentShell
+     */
+    private SubscriptionsDialog(Shell parentShell, SubscriptionManager subscriptionManage) {
+        super(parentShell);
+        setHelpAvailable(false);
+        setShellStyle(SWT.DIALOG_TRIM | SWT.RESIZE | SWT.APPLICATION_MODAL);
+        this.subscriptionManager = subscriptionManage;
+    }
 
-	public static SubscriptionsDialog go(Shell parentShell, SubscriptionManager subscriptionManager) {
-		SubscriptionsDialog d = new SubscriptionsDialog(parentShell, subscriptionManager);
-		if (d.open() == Window.OK) {
-			return d;
-		}
-		return null;
-	}
+    public static SubscriptionsDialog go(Shell parentShell, SubscriptionManager subscriptionManager) {
+        SubscriptionsDialog d = new SubscriptionsDialog(parentShell, subscriptionManager);
+        if (d.open() == Window.OK) {
+            return d;
+        }
+        return null;
+    }
 
-	/**
-	 * Create contents of the dialog.
-	 * @param parent
-	 */
-	@Override
-	protected Control createDialogArea(Composite parent) {
-		setMessage(SUBSCRIPTION_MESSAGE);
-		setTitle(SUBSCRIPTION_TITLE);
-		getShell().setText(DIALOG_TITLE);
-		Composite area = (Composite) super.createDialogArea(parent);
-		Composite container = new Composite(area, SWT.NONE);
-		container.setLayout(new GridLayout(1, false));
-		container.setLayoutData(new GridData(GridData.FILL_BOTH));
+    /**
+     * Create contents of the dialog.
+     * @param parent
+     */
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        setMessage(SUBSCRIPTION_MESSAGE);
+        setTitle(SUBSCRIPTION_TITLE);
+        getShell().setText(DIALOG_TITLE);
+        Composite area = (Composite) super.createDialogArea(parent);
+        Composite container = new Composite(area, SWT.NONE);
+        container.setLayout(new GridLayout(1, false));
+        container.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-		table = new Table(container, SWT.BORDER | SWT.CHECK | SWT.FULL_SELECTION);
-		GridData gdTable = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
-		gdTable.heightHint = 300;
-		table.setLayoutData(gdTable);
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
+        table = new Table(container, SWT.BORDER | SWT.CHECK | SWT.FULL_SELECTION);
+        GridData gdTable = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+        gdTable.heightHint = 300;
+        table.setLayoutData(gdTable);
+        table.setHeaderVisible(true);
+        table.setLinesVisible(true);
 
-		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn.setWidth(300);
-		tblclmnNewColumn.setText("Subscription Name");
+        TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
+        tblclmnNewColumn.setWidth(300);
+        tblclmnNewColumn.setText("Subscription Name");
 
-		TableColumn tblclmnNewColumn1 = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn1.setWidth(270);
-		tblclmnNewColumn1.setText("Subscription ID");
+        TableColumn tblclmnNewColumn1 = new TableColumn(table, SWT.NONE);
+        tblclmnNewColumn1.setWidth(270);
+        tblclmnNewColumn1.setText("Subscription ID");
 
-		Composite composite = new Composite(container, SWT.NONE);
-		composite.setLayout(new RowLayout(SWT.HORIZONTAL));
+        Composite composite = new Composite(container, SWT.NONE);
+        composite.setLayout(new RowLayout(SWT.HORIZONTAL));
 
-		Button btnRefresh = new Button(composite, SWT.NONE);
-		btnRefresh.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				refreshSubscriptions();
-			}
-		});
-		btnRefresh.setText("Refresh");
+        Button btnRefresh = new Button(composite, SWT.NONE);
+        btnRefresh.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                refreshSubscriptions();
+            }
+        });
+        btnRefresh.setText("Refresh");
 
-		Button btnSelectAll = new Button(composite, SWT.NONE);
-		btnSelectAll.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				Arrays.stream(table.getItems()).forEach(d -> {
-					d.setChecked(true);
-				});
-			}
-		});
-		btnSelectAll.setText("Select All");
+        Button btnSelectAll = new Button(composite, SWT.NONE);
+        btnSelectAll.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                Arrays.stream(table.getItems()).forEach(d -> {
+                    d.setChecked(true);
+                });
+            }
+        });
+        btnSelectAll.setText("Select All");
 
-		Button btnDeselectAll = new Button(composite, SWT.NONE);
-		btnDeselectAll.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				Arrays.stream(table.getItems()).forEach(d -> {
-					d.setChecked(false);
-				});
-			}
-		});
-		btnDeselectAll.setText("Deselect All");
-		AccessibilityUtils.addAccessibilityNameForUIComponent(table, "subscriptions");
-		return area;
-	}
+        Button btnDeselectAll = new Button(composite, SWT.NONE);
+        btnDeselectAll.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                Arrays.stream(table.getItems()).forEach(d -> {
+                    d.setChecked(false);
+                });
+            }
+        });
+        btnDeselectAll.setText("Deselect All");
+        AccessibilityUtils.addAccessibilityNameForUIComponent(table, "subscriptions");
+        return area;
+    }
 
-	@Override
-	public void create() {
-		super.create();
-		Display.getDefault().asyncExec(this::setSubscriptions);
-	}
+    @Override
+    public void create() {
+        super.create();
+        Display.getDefault().asyncExec(this::setSubscriptions);
+    }
 
-	public void refreshSubscriptionsAsync() {
+    public void refreshSubscriptionsAsync() {
         try {
             ProgressDialog.get(getShell(), "Update Azure Local Cache Progress").run(true, false, new IRunnableWithProgress() {
                 @Override
@@ -166,84 +166,84 @@ public class SubscriptionsDialog extends AzureTitleAreaDialogWrapper {
             //LOGGER.log(LogService.LOG_ERROR, "run@refreshSubscriptionsAsync@SubscriptionDialog", e);
             LOG.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "run@refreshSubscriptionsAsync@SubscriptionDialog", ex));
         }
-	}
+    }
 
-	private void setSubscriptions() {
-		sdl = subscriptionManager.getSubscriptionDetails();
-		for (SubscriptionDetail sd : sdl) {
-			TableItem item = new TableItem(table, SWT.NULL);
+    private void setSubscriptions() {
+        sdl = subscriptionManager.getSubscriptionDetails();
+        for (SubscriptionDetail sd : sdl) {
+            TableItem item = new TableItem(table, SWT.NULL);
             item.setText(new String[] {sd.getSubscriptionName(), sd.getSubscriptionId()});
             item.setChecked(sd.isSelected());
-		}
-	}
+        }
+    }
 
-	private void refreshSubscriptions() {
-		System.out.println("refreshSubscriptions");
-		table.removeAll();
-		subscriptionManager.cleanSubscriptions();
-		refreshSubscriptionsAsync();
-		setSubscriptions();
-		subscriptionManager.setSubscriptionDetails(sdl);
-	}
+    private void refreshSubscriptions() {
+        System.out.println("refreshSubscriptions");
+        table.removeAll();
+        subscriptionManager.cleanSubscriptions();
+        refreshSubscriptionsAsync();
+        setSubscriptions();
+        subscriptionManager.setSubscriptionDetails(sdl);
+    }
 
-	/**
-	 * Create contents of the button bar.
-	 * @param parent
-	 */
-	@Override
-	protected void createButtonsForButtonBar(Composite parent) {
-		super.createButtonsForButtonBar(parent);
-		Button okButton = getButton(IDialogConstants.OK_ID);
-		okButton.setText("Select");
-	}
+    /**
+     * Create contents of the button bar.
+     * @param parent
+     */
+    @Override
+    protected void createButtonsForButtonBar(Composite parent) {
+        super.createButtonsForButtonBar(parent);
+        Button okButton = getButton(IDialogConstants.OK_ID);
+        okButton.setText("Select");
+    }
 
-	@Override
-	public void okPressed() {
-		EventUtil.logEvent(EventType.info, ACCOUNT, SELECT_SUBSCRIPTIONS, null);
-		TableItem[] tia = table.getItems();
-		int rc = tia.length;
-		int chekedCount = 0;
-		for (TableItem ti : tia) {
-			if (ti.getChecked()) {
-				chekedCount++;
-			}
-		}
+    @Override
+    public void okPressed() {
+        EventUtil.logEvent(EventType.info, ACCOUNT, SELECT_SUBSCRIPTIONS, null);
+        TableItem[] tia = table.getItems();
+        int rc = tia.length;
+        int chekedCount = 0;
+        for (TableItem ti : tia) {
+            if (ti.getChecked()) {
+                chekedCount++;
+            }
+        }
 
-		if (chekedCount == 0) {
-			this.setErrorMessage("Select at least one subscription");
-			return;
-		}
+        if (chekedCount == 0) {
+            this.setErrorMessage("Select at least one subscription");
+            return;
+        }
 
-		for (int i = 0; i < tia.length; ++i) {
-			this.sdl.get(i).setSelected(tia[i].getChecked());
-		}
+        for (int i = 0; i < tia.length; ++i) {
+            this.sdl.get(i).setSelected(tia[i].getChecked());
+        }
 
-		try {
-			subscriptionManager.setSubscriptionDetails(sdl);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			LOG.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "okPressed@SubscriptionDialog", ex));
-		}
+        try {
+            subscriptionManager.setSubscriptionDetails(sdl);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            LOG.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "okPressed@SubscriptionDialog", ex));
+        }
 
-		List<String> selectedIds = this.sdl.stream().filter(SubscriptionDetail::isSelected)
-				.map(SubscriptionDetail::getSubscriptionId).collect(Collectors.toList());
-		IdentityAzureManager.getInstance().selectSubscriptionByIds(selectedIds);
+        List<String> selectedIds = this.sdl.stream().filter(SubscriptionDetail::isSelected)
+                .map(SubscriptionDetail::getSubscriptionId).collect(Collectors.toList());
+        IdentityAzureManager.getInstance().selectSubscriptionByIds(selectedIds);
 
-		IdentityAzureManager.getInstance().getSubscriptionManager().notifySubscriptionListChanged();
-		Mono.fromCallable(() -> {
-			AzureAccount az = Azure.az(AzureAccount.class);
-			selectedIds.stream().limit(5).forEach(sid -> {
-				// pr-load regions
-				az.listRegions(sid);
-			});
-			return 1;
-		}).subscribeOn(Schedulers.boundedElastic()).subscribe();
+        IdentityAzureManager.getInstance().getSubscriptionManager().notifySubscriptionListChanged();
+        Mono.fromCallable(() -> {
+            AzureAccount az = Azure.az(AzureAccount.class);
+            selectedIds.stream().limit(5).forEach(sid -> {
+                // pr-load regions
+                az.listRegions(sid);
+            });
+            return 1;
+        }).subscribeOn(Schedulers.boundedElastic()).subscribe();
 
-		final Map<String, String> properties = new HashMap<>();
-		properties.put("subsCount", String.valueOf(rc));
-		properties.put("selectedSubsCount", String.valueOf(chekedCount));
-		EventUtil.logEvent(EventType.info, ACCOUNT, SELECT_SUBSCRIPTIONS, null);
+        final Map<String, String> properties = new HashMap<>();
+        properties.put("subsCount", String.valueOf(rc));
+        properties.put("selectedSubsCount", String.valueOf(chekedCount));
+        EventUtil.logEvent(EventType.info, ACCOUNT, SELECT_SUBSCRIPTIONS, null);
 
-		super.okPressed();
-	}
+        super.okPressed();
+    }
 }
