6c7efb75821052d01f7c1b278ee695065984e4d2
Njeri Mwenjwa <joymwenjwa@gmail.com>
2024-06-26 09:47:39 -0700

    refactor rule & add tests

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-azure-sdk/src/main/java/com/microsoft/azure/toolkit/intellij/azure/sdk/buildtool/ConnectionStringCheck.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-azure-sdk/src/main/java/com/microsoft/azure/toolkit/intellij/azure/sdk/buildtool/ConnectionStringCheck.java
@@ -3,27 +3,88 @@ package com.microsoft.azure.toolkit.intellij.azure.sdk.buildtool;
 import com.intellij.codeInspection.*;
 import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * This class extends the LocalInspectionTool and checks for the usage of connection strings in the code.
- * Connection strings that start with "DefaultEndpointsProtocol=http;AccountName=" are flagged as problems.
- * This is because the use of such connection strings is not recommended.
+ * This class extends the LocalInspectionTool and checks for the usage of connection strings API to create clients
+ * in the Azure SDK for Java.
+ * If a connection string is found, a warning is displayed.
  */
 public class ConnectionStringCheck extends LocalInspectionTool {
+
+    private static final List <String> ruleConfigurations = loadRuleConfigurations();
+
     @NotNull
     @Override
     public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
         return new JavaElementVisitor() {
+
             @Override
-            public void visitLiteralExpression(PsiLiteralExpression expression) {
-                super.visitLiteralExpression(expression);
+            public void visitElement(@NotNull PsiElement element) {
+                super.visitElement(element);
 
-                String value = expression.getValue() instanceof String ? (String) expression.getValue() : null;
+                String METHOD_TO_CHECK = ruleConfigurations.get(0);
+                String SUGGESTION = ruleConfigurations.get(1);
 
-                if (value != null && value.startsWith("DefaultEndpointsProtocol=http;AccountName=")) {
-                    holder.registerProblem(expression, "Use of connection strings is not recommended");
+                // check if an element is an azure client
+                if (element instanceof PsiMethodCallExpression) {
+
+                    // check if the method is a connectionString call
+                    PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) element;
+                    PsiReferenceExpression methodExpression = methodCallExpression.getMethodExpression();
+
+                    // resolvedMethod is the method that is being called
+                    PsiElement resolvedMethod = methodExpression.resolve();
+
+                    if (resolvedMethod != null && resolvedMethod instanceof PsiElement && ((PsiMethod) resolvedMethod).getName().equals(METHOD_TO_CHECK)) {
+                        PsiMethod method = (PsiMethod) resolvedMethod;
+
+                        // containingClass is the client class that is being called
+                        PsiClass containingClass = method.getContainingClass();
+
+                        // check if the class is an azure client
+                        if (containingClass != null && containingClass.getQualifiedName() != null && containingClass.getQualifiedName().startsWith("com.azure")) {
+                            holder.registerProblem(element, SUGGESTION);
+                        }
+                    }
                 }
             }
         };
+    };
+
+    // Load the rule configurations from the configuration file
+    private static List<String> loadRuleConfigurations() {
+
+        final String RULE_CONFIGURATION = "META-INF/ruleConfigs.json";
+        final String RULE_NAME = "ConnectionStringCheck";
+        final String METHOD_TO_CHECK_KEY = "method_to_check";
+        final String SUGGESTION_KEY = "antipattern_message";
+
+        try {
+            InputStream inputStream = ConnectionStringCheck.class.getClassLoader().getResourceAsStream(RULE_CONFIGURATION);
+            if (inputStream == null) {
+                throw new FileNotFoundException("Configuration file not found");
+            }
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+                final JSONObject jsonObject = new JSONObject(bufferedReader.lines().collect(Collectors.joining()));
+                JSONObject ruleConfig = jsonObject.getJSONObject(RULE_NAME);
+                String methodToCheck = ruleConfig.getString(METHOD_TO_CHECK_KEY);
+                String antipatternMessage = ruleConfig.getString(SUGGESTION_KEY);
+                return Arrays.asList(methodToCheck, antipatternMessage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
     }
 }
---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-azure-sdk/src/test/java/com/microsoft/azure/toolkit/intellij/azure/sdk/buildtool/ConnectionStringCheckTest.java
@@ -0,0 +1,135 @@
+package com.microsoft.azure.toolkit.intellij.azure.sdk.buildtool;
+
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.JavaElementVisitor;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiMethodCallExpression;
+import com.intellij.psi.PsiReferenceExpression;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * This class tests the ConnectionStringCheck class.
+ * It tests the buildVisitor method and the visitElement method.
+ * It tests the check for the usage of connection strings API to create clients in the Azure SDK for Java.
+ *
+ * This
+ */
+public class ConnectionStringCheckTest {
+
+    @Mock
+    private ProblemsHolder mockHolder;
+
+    @Mock
+    private PsiElement mockElement;
+
+    @Mock
+    private PsiElementVisitor mockVisitor;
+
+
+    @BeforeEach
+    public void setup() {
+        mockHolder = mock(ProblemsHolder.class);
+        mockElement = mock(PsiElement.class);
+        mockVisitor = createVisitor();
+    }
+
+    PsiElementVisitor createVisitor() {
+        boolean isOnTheFly = true;
+        ConnectionStringCheck check = new ConnectionStringCheck();
+        return check.buildVisitor(mockHolder, isOnTheFly);
+    }
+
+    /**
+     * This test checks the buildVisitor method.
+     * It checks if the visitor is created and if it is an instance of JavaElementVisitor.
+     * It also verifies that a warning is raised when a connection string being used to create a client is detected.
+     *
+     * This is an example of a passing test.
+     * 1. ServiceBusSenderClient senderClient2 = new ServiceBusClientBuilder().connectionString(connectionString2).sender().queueName("myQueueName2").buildClient();
+     * 2. BlobServiceClient blobServiceClient2 = new BlobServiceClientBuilder().connectionString(connectionString2).buildClient();
+     */
+    @Test
+    void testConnectionStringCheck() {
+
+        assertVisitor(mockVisitor);
+        String METHOD_TO_CHECK = "connectionString";
+        int NUMBER_OF_INVOCATIONS = 1;
+        String CLASS_NAME = "com.azure";
+
+        verifyRegisterProblem(mockVisitor, METHOD_TO_CHECK, NUMBER_OF_INVOCATIONS, CLASS_NAME);
+    }
+
+
+    // Asserts that the visitor is not null and is an instance of JavaElementVisitor
+    void assertVisitor(PsiElementVisitor visitor) {
+        assertNotNull(visitor);
+        assertTrue(visitor instanceof JavaElementVisitor);
+    }
+
+    // Verifies that a warning is raised when a connection string being used to create a client is detected.
+    void verifyRegisterProblem(PsiElementVisitor visitor, String METHOD_TO_CHECK, int NUMBER_OF_INVOCATIONS, String CLASS_NAME) {
+
+        PsiMethodCallExpression methodCallExpression = mock(PsiMethodCallExpression.class);
+        PsiReferenceExpression methodExpression = mock(PsiReferenceExpression.class);
+        PsiMethod resolvedMethod = mock(PsiMethod.class);
+        PsiMethod method = mock(PsiMethod.class);
+        PsiClass containingClass = mock(PsiClass.class);
+
+        when(methodCallExpression.getMethodExpression()).thenReturn(methodExpression);
+        when(methodExpression.resolve()).thenReturn(resolvedMethod);
+        when(resolvedMethod.getContainingClass()).thenReturn(containingClass);
+        when(resolvedMethod.getName()).thenReturn(METHOD_TO_CHECK);
+        when(containingClass.getQualifiedName()).thenReturn(CLASS_NAME);
+
+        (visitor).visitElement(methodCallExpression);
+
+        // Verify problem is registered
+        verify(mockHolder, times(NUMBER_OF_INVOCATIONS)).registerProblem(Mockito.eq(methodCallExpression), Mockito.contains("Connection String detected. Use AzureKeyCredentials for azure service client authentication instead."));
+    }
+
+
+    // Problem isn't registered because the method to check is different from the method that should be flagged
+    @Test
+    void differentMethodCheck() {
+        String METHOD_TO_CHECK = "differentMethod";
+        int NUMBER_OF_INVOCATIONS = 0;
+        String CLASS_NAME = "com.azure";
+
+        verifyRegisterProblem(mockVisitor, METHOD_TO_CHECK, NUMBER_OF_INVOCATIONS, CLASS_NAME);
+    }
+
+    // Problem isn't registered because the class name is different from the class that should be flagged
+    @Test
+    void differentClassCheck() {
+        String METHOD_TO_CHECK = "connectionString";
+        int NUMBER_OF_INVOCATIONS = 0;
+        String CLASS_NAME = "com.microsoft.azure";
+
+        verifyRegisterProblem(mockVisitor, METHOD_TO_CHECK, NUMBER_OF_INVOCATIONS, CLASS_NAME);
+    }
+
+    // Problem isn't registered because the class name is null
+    @Test
+    void nullClassCheck() {
+        String METHOD_TO_CHECK = "connectionString";
+        int NUMBER_OF_INVOCATIONS = 0;
+        String CLASS_NAME = null;
+
+        verifyRegisterProblem(mockVisitor, METHOD_TO_CHECK, NUMBER_OF_INVOCATIONS, CLASS_NAME);
+    }
+}
