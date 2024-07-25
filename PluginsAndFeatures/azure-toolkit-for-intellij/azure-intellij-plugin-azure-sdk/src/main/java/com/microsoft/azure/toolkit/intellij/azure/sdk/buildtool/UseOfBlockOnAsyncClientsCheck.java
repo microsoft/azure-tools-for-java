02a8cf28cb78f74ace8184717d3c58c47e722897
Njeri Mwenjwa <joymwenjwa@gmail.com>
2024-07-24 21:06:36 -0700

    update to implementation

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-azure-sdk/src/main/java/com/microsoft/azure/toolkit/intellij/azure/sdk/buildtool/UseOfBlockOnAsyncClientsCheck.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-azure-sdk/src/main/java/com/microsoft/azure/toolkit/intellij/azure/sdk/buildtool/UseOfBlockOnAsyncClientsCheck.java
@@ -4,10 +4,12 @@ import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.JavaElementVisitor;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiClassType;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.PsiMethodCallExpression;
 
 import com.intellij.psi.PsiType;
+import com.intellij.psi.util.InheritanceUtil;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
@@ -63,7 +65,8 @@ public class UseOfBlockOnAsyncClientsCheck extends LocalInspectionTool {
 
         /**
          * Constructor to initialize the visitor with the holder and isOnTheFly flag.
-         * @param holder ProblemsHolder
+         *
+         * @param holder     ProblemsHolder
          * @param isOnTheFly boolean
          */
         public UseOfBlockOnAsyncClientsVisitor(ProblemsHolder holder, boolean isOnTheFly) {
@@ -74,6 +77,7 @@ public class UseOfBlockOnAsyncClientsCheck extends LocalInspectionTool {
         /**
          * Visit method call expressions to check for the use of block() method on async clients.
          * This method will check if the method call is block() and if it is used in an async context.
+         *
          * @param expression
          */
         @Override
@@ -87,9 +91,9 @@ public class UseOfBlockOnAsyncClientsCheck extends LocalInspectionTool {
                 // Check if the method call is block() or a variant of block() method
                 if (methodCall.getMethodExpression().getReferenceName().startsWith("block")) {
                     boolean isAsyncContext = checkIfAsyncContext(methodCall);
-
-                    // Check if the method call is on an Azure SDK client and is in an async context
-                    if (isAsyncContext && isAzureClient(methodCall)) {
+                    System.out.println("isAsyncContext: " + isAsyncContext);
+                    if (isAsyncContext) {
+                        // Report the problem
                         holder.registerProblem(expression, ANTI_PATTERN_MESSAGE);
                     }
                 }
@@ -99,25 +103,58 @@ public class UseOfBlockOnAsyncClientsCheck extends LocalInspectionTool {
         /**
          * Helper method to check if the method call is within an async context.
          * This method will check if the method call is on a reactive type like Mono, Flux, etc.
+         *
          * @param methodCall
          * @return true if the method call is on a reactive type, false otherwise
          */
         private boolean checkIfAsyncContext(@NotNull PsiMethodCallExpression methodCall) {
-            PsiExpression expression = methodCall.getMethodExpression().getQualifierExpression();
+            PsiExpression qualifierExpression = methodCall.getMethodExpression().getQualifierExpression();
 
-            // Check if the method call is on a reactive type
-            if (expression != null) {
-                PsiType type = expression.getType();
+            System.out.println("qualifierExpression: " + qualifierExpression);
 
-                // Check if the type is a reactive type
-                if (type != null) {
-                    String typeName = type.getCanonicalText();
+            if (qualifierExpression instanceof PsiMethodCallExpression) {
+                PsiMethodCallExpression qualifierMethodCall = (PsiMethodCallExpression) qualifierExpression;
+                System.out.println("qualifierMethodCall: " + qualifierMethodCall);
 
-                    // Check for common async/reactive types directly in the list asyncReturnTypes
-                    for (String asyncReturnType : ASYNC_RETURN_TYPES) {
-                        if (typeName.contains(asyncReturnType)) {
+                // Get the type of the qualifier method call
+                PsiType qualifierReturnType = qualifierMethodCall.getType();
+                System.out.println("qualifierReturnType: " + qualifierReturnType);
+
+                if (qualifierReturnType instanceof PsiClassType) {
+                    PsiClass qualifierReturnTypeClass = ((PsiClassType) qualifierReturnType).resolve();
+                    System.out.println("qualifierReturnTypeClass: " + qualifierReturnTypeClass);
+
+                    if (qualifierReturnTypeClass != null && isSubclassOfMonoOrFlux(qualifierReturnTypeClass)) {
+
+                        if (isAzureAsyncClient(qualifierMethodCall)) {
+                            System.out.println("isAzureAsyncClient: " + qualifierMethodCall);
+
+                            // Report the problem
                             return true;
                         }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private boolean isAzureAsyncClient(PsiMethodCallExpression qualifierMethodCall) {
+
+            // Get the expression that calls the method returning a reactive type
+            PsiExpression clientExpression = qualifierMethodCall.getMethodExpression().getQualifierExpression();
+            System.out.println("clientExpression: " + clientExpression);
+
+            if (clientExpression != null) {
+                PsiType clientType = clientExpression.getType();
+                System.out.println("clientType: " + clientType);
+
+                if (clientType instanceof PsiClassType) {
+                    PsiClass clientClass = ((PsiClassType) clientType).resolve();
+                    System.out.println("clientClass: " + clientClass);
+
+                    if (clientClass != null && clientClass.getQualifiedName().startsWith("com.azure.") && clientClass.getQualifiedName().endsWith("AsyncClient")) {
+                        return true;
                     }
                 }
             }
@@ -125,32 +162,25 @@ public class UseOfBlockOnAsyncClientsCheck extends LocalInspectionTool {
         }
 
         /**
-         * Helper method to check if the method call is on an Azure SDK client.
-         * This method will check if the method call is on a class that is part of the Azure SDK.
-         * @param methodCall
-         * @return true if the method call is on an Azure SDK client, false otherwise
+         * Helper method to check if the class is a subclass of Mono or Flux.
+         *
+         * @param psiClass
+         * @return true if the class is a subclass of Mono or Flux, false otherwise
          */
-        private boolean isAzureClient(@NotNull PsiMethodCallExpression methodCall) {
-            PsiClass containingClass = PsiTreeUtil.getParentOfType(methodCall, PsiClass.class);
-
-            // Check if the method call is on a class
-            if (containingClass != null) {
-                String className = containingClass.getQualifiedName();
-
-                // Check if the class is part of the Azure SDK
-                if (className != null && className.startsWith("com.azure.")) {
-                    return true;
-                }
-            }
-            return false;
+        private boolean isSubclassOfMonoOrFlux(PsiClass psiClass) {
+            System.out.println("psiClass: " + psiClass);
+            return InheritanceUtil.isInheritor(psiClass, "reactor.core.publisher.Mono") ||
+                    InheritanceUtil.isInheritor(psiClass, "reactor.core.publisher.Flux");
         }
 
-        /** helper method to load config from json
+
+        /**
+         * helper method to load config from json
          *
          * @return Map<String, Object>
          * @throws IOException
          */
-        private static  Map<String, Object> getAsyncReturnTypesToCheck() throws IOException {
+        private static Map<String, Object> getAsyncReturnTypesToCheck() throws IOException {
 
             //load json object
             JSONObject jsonObject = LoadJsonConfigFile.getInstance().getJsonObject();
