9b1d8310eb84a2f0316a95f462ded9590b765f93
Hanxiao Liu <hanli@microsoft.com>
2024-10-15 15:39:32 +0800

    Upgrade intellij gradle plugin to stable version

--- PluginsAndFeatures/azure-toolkit-for-intellij/build.gradle.kts
+++ PluginsAndFeatures/azure-toolkit-for-intellij/build.gradle.kts
@@ -1,5 +1,6 @@
 import io.freefair.gradle.plugins.aspectj.AjcAction
 import org.apache.tools.ant.filters.ReplaceTokens
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.net.URL
@@ -281,9 +282,12 @@ tasks {
         jvmArgs("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005")
     }
 
-//    val runLocalIde by registering(CustomRunIdeTask::class) {
-//        localPath = file("C:\\Program Files\\JetBrains\\IntelliJ IDEA Community Edition 2024.1")
-//    }
+    // refers https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-testing-extension.html#intellijPlatformTesting
+    val testIde by intellijPlatformTesting.runIde.registering {
+        type = IntelliJPlatformType.IntellijIdeaCommunity
+        version = "2024.2"
+    }
+
     // Configure UI tests plugin
     // Read more: https://github.com/JetBrains/intellij-ui-test-robot
     //    testIdeUi {
